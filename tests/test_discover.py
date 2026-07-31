1d73c5d852665e11641ea1ca9165ef96e4efd667
Ptheusen Geagoni <pgeagoni@gmail.com>
2026-07-31 20:21:29 +0800

    test: pin deduplication and numeric sorting in section discovery

--- tests/test_discover.py
+++ tests/test_discover.py
@@ -12,3 +12,14 @@ def test_lists_subpart_d_sections_in_numeric_order():
         "1910.21", "1910.22", "1910.23", "1910.24", "1910.25",
         "1910.26", "1910.27", "1910.28", "1910.29", "1910.30",
     ]
+
+
+def test_deduplicates_and_sorts_numerically():
+    html = """
+    <a href="/laws-regs/regulations/standardnumber/1910/1910.147">loto</a>
+    <a href="/laws-regs/regulations/standardnumber/1910/1910.9">low number</a>
+    <a href="/laws-regs/regulations/standardnumber/1910/1910.147">duplicate link</a>
+    <a href="/laws-regs/regulations/standardnumber/1910/1910.23">ladders</a>
+    """
+
+    assert section_ids_from_subpart(html) == ["1910.9", "1910.23", "1910.147"]
